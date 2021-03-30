f31f03244bd747766683cff0812248c0cfc5a092
K.Korey Wertman <kkwertman@mail.fhsu.edu>
2021-03-30 16:04:10 -0400

    Create game function, next work on  game win logic

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-//Selction variables 
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
+function game() {
+    //Selction variables 
+    const computerSelection = computerPlay();
+    const playerSelection = playerPlay();
 
-//Prompt, validate, and store user's play hand
-function playerPlay() {
-    let playerInput = prompt("Please chose between 'rock', 'paper' or 'scissors'");
-    if (playerInput.toLowerCase() !== 'rock' && playerInput.toLowerCase() !== 'scissors' && playerInput.toLowerCase() !== 'paper'){
-        return playerPlay();
-    } else {
-        return playerInput;
+    //Prompt, validate, and store user's play hand
+    function playerPlay() {
+        let playerInput = prompt("Please chose between 'rock', 'paper' or 'scissors'");
+        if (playerInput.toLowerCase() !== 'rock' && playerInput.toLowerCase() !== 'scissors' && playerInput.toLowerCase() !== 'paper') {
+            return playerPlay();
+        } else {
+            return playerInput.toLowerCase();
+        }
+    };
+
+    //Generates the computer's selection
+    function computerPlay() {
+        let compRandom = function getRandomInt() {
+            let rand = Math.floor((Math.random() * 3) + 1);
+            return rand;
+        };
+        let compInput = compRandom();
+        if (compInput == 1) {
+            compInput = "rock";
+        } else if (compInput == 2) {
+            compInput = "paper";
+        } else {
+            compInput = "scissors";
+        }
+        return compInput;
     }
-};
 
-
-//Generates the computer's selection
-function computerPlay () {
-    let compRandom = function getRandomInt() {
-        let rand = Math.floor((Math.random() * 3) + 1);
-        return rand;
-     };
-     let compInput = compRandom();
-    if (compInput == 1){
-        compInput = "rock";
-    } else if (compInput == 2)  {
-        compInput = "paper";
-    } else {
-        compInput = "scissors";
-    }
-    return compInput;
+    //Determine the winner
+    console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}`);
 }
