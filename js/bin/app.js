3e8f0a0c22fd5aafdfc23e853d2fc3638320a61b
K.Korey Wertman <kkwertman@mail.fhsu.edu>
2021-04-13 10:22:30 -0400

    continue to work on game logic

--- js/bin/app.js
+++ js/bin/app.js
@@ -1,6 +1,6 @@
 //UI variables
 const playBtn = document.querySelector("#start-play"),
-    playerBtnChoice = document.querySelectorAll(".playerBtn")
+    playerBtnChoice = document.querySelectorAll(".playerBtn"),
     playerScore = document.querySelector("#player-score"),
     computerScore = document.querySelector("#cpu-score"),
     tieScore = document.querySelector("#tie-score");
@@ -10,29 +10,17 @@ const playBtn = document.querySelector("#start-play"),
 let playerWin = 0,
     computerWin = 0,
     tie = 0,
+    round = 0,
     gameOver = false;
 
 
+//Start round with player selection
+playerBtnChoice.forEach(button => {
+    button.addEventListener('click', () => {
+        const playerSelection = button.getAttribute('id');
+    });
 
-playBtn.addEventListener('click', function () {
-    //Selction result variables 
-    const computerSelection = playComputer();
-    const playerSelection = playPlayer();
 
-    //Get player input
-    //Prompt, validate, and store user's play hand
-    function playPlayer() {
-        array.forEach(element => {
-            
-        });
-        
-        playerInput = prompt("Please chose between 'rock', 'paper' or 'scissors'");
-        if (playerInput.toLowerCase() !== 'rock' && playerInput.toLowerCase() !== 'scissors' && playerInput.toLowerCase() !== 'paper') {
-            return playPlayer();
-        } else {
-            return playerInput.toLowerCase();
-        }
-    };
 
     //Generates the computer's selection
     function playComputer() {
@@ -51,63 +39,70 @@ playBtn.addEventListener('click', function () {
         return compInput;
     }
 
-    //Determine the winner
-    function showRoundResults() {
-        if (playerSelection == 'rock' && computerSelection == 'rock') {
-            tie++
-            tieScore.textContent = `Ties: ${tie}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. It's a tie round!`);
-        } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-            computerWin++
-            computerScore.textContent = `Computer: ${computerWin}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. Computer wins this round!`);
-        } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-            playerWin++
-            playerScore.textContent = `Player: ${playerWin}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. You win this round!`);
-        } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-            playerWin++
-            playerScore.textContent = `Player: ${playerWin}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. You win this round!`);
-        } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-            tie++
-            tieScore.textContent = `Ties: ${tie}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. It's a tie round!`);
-        } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-            computerWin++
-            computerScore.textContent = `Computer: ${computerWin}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. Computer wins this round!`);
-        } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-            computerWin++
-            computerScore.textContent = `Computer: ${computerWin}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. Computer wins this round!`);
-        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-            playerWin++
-            playerScore.textContent = `Player: ${playerWin}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. You win this round!`);
-        } else {
-            tie++
-            tieScore.textContent = `Ties: ${tie}`;
-            console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. It's a tie round!`);
-        }
-        if (playerWin + computerWin + tie === 5)
-            gameOver = true;
-        if (gameOver === true) {
-            //Compare and tally round results. Declare an overall game winner. 
-            function getGameResults() {
-                if (playerWin > computerWin) {
-                    console.log(`You won ${playerWin}x and the computer won ${computerWin}x. YOU are the CHAMPION!!!`);
-                } else if (computerWin > playerWin) {
-                    console.log(`You won ${playerWin}x and the computer won ${computerWin}x. YOU LOSE!!!`);
-                } else {
-                    console.log(`You won ${playerWin}x and the computer won ${computerWin}x. It's a TIE!!!`);
-                }
-            }
-            return getGameResults();
-        };
-    }
-    //Print the results of each round
-    return showRoundResults();
-
 });
 
+
+
+
+
+
+
+
+
+//Determine the winner
+function showRoundResults() {
+    if (playerSelection == 'rock' && computerSelection == 'rock') {
+        tie++
+        tieScore.textContent = `Ties: ${tie}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. It's a tie round!`);
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerWin++
+        computerScore.textContent = `Computer: ${computerWin}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. Computer wins this round!`);
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerWin++
+        playerScore.textContent = `Player: ${playerWin}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. You win this round!`);
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerWin++
+        playerScore.textContent = `Player: ${playerWin}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. You win this round!`);
+    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+        tie++
+        tieScore.textContent = `Ties: ${tie}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. It's a tie round!`);
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerWin++
+        computerScore.textContent = `Computer: ${computerWin}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. Computer wins this round!`);
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerWin++
+        computerScore.textContent = `Computer: ${computerWin}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. Computer wins this round!`);
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerWin++
+        playerScore.textContent = `Player: ${playerWin}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. You win this round!`);
+    } else {
+        tie++
+        tieScore.textContent = `Ties: ${tie}`;
+        console.log(`You threw ${playerSelection} and the computer threw ${computerSelection}. It's a tie round!`);
+    }
+    if (playerWin + computerWin + tie === 5)
+        gameOver = true;
+    if (gameOver === true) {
+        //Compare and tally round results. Declare an overall game winner. 
+        function getGameResults() {
+            if (playerWin > computerWin) {
+                console.log(`You won ${playerWin}x and the computer won ${computerWin}x. YOU are the CHAMPION!!!`);
+            } else if (computerWin > playerWin) {
+                console.log(`You won ${playerWin}x and the computer won ${computerWin}x. YOU LOSE!!!`);
+            } else {
+                console.log(`You won ${playerWin}x and the computer won ${computerWin}x. It's a TIE!!!`);
+            }
+        }
+        return getGameResults();
+    };
+    //Print the results of each round
+    return showRoundResults();
+}
